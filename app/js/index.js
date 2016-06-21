350c2fdae90dd138ab60b0e1c7672e4d970df8bb
whao <whaodu@163.com>
2016-06-21 15:23:28 +0800

    背景

--- app/js/index.js
+++ app/js/index.js
@@ -17,4 +17,31 @@
         // nextButton: '.swiper-button-next',
         // prevButton: '.swiper-button-prev'
     });
-    
+
+
+var changePosition = function(){
+    var page = $('.page-1');
+    var $content = $('.content');
+    var swiper_page = $('.swiper-bg');
+
+    $content.on('mousemove',function(event){
+        mouseFloat(event,swiper_page);
+    })
+};
+
+var mouseFloat = function(event,el){
+    var x = event.clientX;
+    var y = event.clientY;
+    var height = el.height();
+    var width = el.width();
+    var top = (height/2-y)/50;
+    var left = (width/2-x)/50;
+    el.css("top",top+'px');
+    el.css("left",left+'px');
+};
+
+
+
+    (function(){
+        changePosition();
+    })()
