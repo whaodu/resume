de441fc5ee8e8fdb1ba062465e21ff161906135d
whao <whaodu@163.com>
2016-06-22 17:50:55 +0800

    修改路径

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ gulp.task('server',['sass'],function(){
 
 	// gulp.watch("build")
 	gulp.watch("app/scss/*.scss", ['sass']);
-	gulp.watch(["app/*html","app/pages/*html","app/js/*js"]).on('change',reload);
+	gulp.watch(["app/*html","app/pages/*html","app/js/*js","*html"]).on('change',reload);
 
 })
 
